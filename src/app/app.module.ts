d8b397b5a5b22d7b9792033938b51b4e223fcb37
Ilya Kousaev <kosaev@gmail.com>
2017-10-18 01:12:02 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
+import { FwModule } from './../fw/fw.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    FwModule
   ],
   providers: [],
   bootstrap: [AppComponent]
